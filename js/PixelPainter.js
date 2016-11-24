63743ac3e300def64cf530ed50e668cb8a14670b
David Ebinger <davidebinger888@gmail.com>
2016-11-24 13:53:01 -1000

    initial commit to post for github pages

---
+++ js/PixelPainter.js
@@ -0,0 +1,104 @@
+
+let paintContainer = document.querySelector("#pixelPainter");
+let cellColor = '';
+function createGrid() {
+
+    let grid = document.getElementById("colorSelector");
+    if (grid) {
+        grid.parentNode.removeChild(grid);
+    }
+
+    let gridTotal = 10;
+    let size = 25;
+
+    let container = document.createElement("div");
+    container.id = "colorSelector";
+    container.className = "container";
+    container.style.width = (gridTotal * size) + "px";
+    container.style.height = (gridTotal * size) + "px";
+
+    for (let i = 0, len = gridTotal * gridTotal; i < len; i++) {
+        grid = document.createElement("div");
+        grid.className = "cell";
+        grid.style.height = size + "px";
+        grid.style.width = size + "px";
+        grid.addEventListener("click",function() {
+        cellColor = this.style.backgroundColor});
+        grid.style.backgroundColor = magic();
+        container.appendChild(grid);
+    }
+    paintContainer.appendChild(container);
+  }
+
+
+function magic()
+
+  {let r = function () { return Math.floor(Math.random()*256);};
+    return "rgb(" + r() + "," + r() + "," + r() + ")";
+  }
+
+createGrid();
+
+function createCanvas() {
+
+    let canvas = document.getElementById("randomBlock");
+    if (canvas) {
+        canvas.parentNode.removeChild(canvas);
+    }
+
+    let canvasTotal = 50;
+
+    let pixelSize = 15;
+
+    let insideCanvas = document.createElement("div");
+    insideCanvas.id = "randomBlock";
+    insideCanvas.className = "insideCanvas";
+    insideCanvas.style.width = (canvasTotal * pixelSize) + "px";
+    insideCanvas.style.height = (canvasTotal * pixelSize) + "px";
+
+    for (let i = 0, len = canvasTotal * canvasTotal; i < len; i++) {
+       canvas = document.createElement("div");
+       canvas.className = "canvasCells";
+       canvas.style.height = pixelSize + "px";
+       canvas.style.width = pixelSize + "px";
+       canvas.addEventListener("click",function() {
+       this.style.backgroundColor = cellColor});
+       canvas.style.backgroundColor = 'white';
+       insideCanvas.appendChild(canvas);
+       canvas.addEventListener('mouseover', function(){
+       if (event.buttons === 1) {
+           if (event.shiftKey) {
+             this.style.backgroundColor = 'white';
+           } else {
+             this.style.backgroundColor = cellColor;
+           }
+         }
+       });
+      }
+       paintContainer.appendChild(insideCanvas);
+ }
+
+  createCanvas();
+
+let eraseButton = document.createElement("button");
+ eraseButton.className = "eraseBtn";
+ eraseButton.innerHTML = "ERASE";
+ paintContainer.appendChild(eraseButton);
+ eraseButton.addEventListener("click", eraseCell);
+
+ function eraseCell() {
+       cellColor = "#FFFFFF";
+ }
+
+
+ let clearButton = document.createElement("button");
+ clearButton.className = "clearBtn";
+ clearButton.innerHTML = "CLEAR";
+ paintContainer.appendChild(clearButton);
+ clearButton.addEventListener("click", clearCanvas);
+
+ function clearCanvas() {
+  for (var i = 0; i < document.querySelectorAll('.canvasCells').length; i++){
+    document.querySelectorAll('.canvasCells')[i].style.backgroundColor = 'white';
+  }
+ }
